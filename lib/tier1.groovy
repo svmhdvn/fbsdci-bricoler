6e3e3ec5aef164e429910a8ee75fbc806aabeda5
Siva Mahadevan <me@svmhdvn.name>
2026-07-03 20:45:55 -0400

    bak

---
+++ lib/tier1.groovy
@@ -0,0 +1,38 @@
+def tinderbox = load('tinderbox.groovy')
+
+def call(String branch) {
+  pipeline {
+    agent { label 'builder' }
+    stages {
+      stage('checkout') {
+        steps {
+          dir ("/exws/${JOB_NAME}/src") {
+            git url: 'ssh://siva@jailhost/home/siva/f/${branch}', branch: "${branch}", poll: false
+          }
+        }
+      }
+      stage('build') {
+        steps {
+          dir ("/exws/${JOB_NAME}/obj") {
+            tinderbox ['amd64', 'arm64', 'riscv']
+          }
+        }
+      }
+      stage('test') {
+        parallel {
+          stage('amd64') {
+            steps { build "siva-${branch}-amd64-test" }
+          }
+          //stage('aarch64') {
+          //  steps { build "siva-${branch}-aarch64-test" }
+          //}
+          //stage('riscv64') {
+          //  steps { build "siva-${branch}-riscv64-test" }
+          //}
+        }
+      }
+    }
+  }
+}
+
+return this
