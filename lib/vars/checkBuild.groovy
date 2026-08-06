b9c95d30e16f9715c9378d6e6d3605a18d6b69cd
Siva Mahadevan <me@svmhdvn.name>
2026-08-06 14:15:28 -0400

    fix groovy syntax

--- lib/vars/checkBuild.groovy
+++ lib/vars/checkBuild.groovy
@@ -8,7 +8,7 @@ def call(String target, String targetArch) {
           script {
             if (fileExists('_.tinderbox.failed')) {
               def matcher = readFile('_.tinderbox.failed') =~ "${target}\\.${targetArch} (\\S*) failed, check"
-              def errorLogs = matcher.map { m -> readFile("_.${target}.${targetArch}.${m[1]}") }.join('\n')
+              def errorLogs = matcher.collect { readFile("_.${target}.${targetArch}.${it[1]}") }.join('\n')
               error(errorLogs)
             }
           }
