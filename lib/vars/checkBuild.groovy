cc3d6b1da12ad03b38013c0fdbce0da703d37b34
Siva Mahadevan <me@svmhdvn.name>
2026-08-06 15:54:55 -0400

    better checkBuild

--- lib/vars/checkBuild.groovy
+++ lib/vars/checkBuild.groovy
@@ -4,12 +4,18 @@ def call(String target, String targetArch) {
     stages {
       stage('check build') {
         steps {
-          copyArtifacts filter: '_.*', projectName: "ci/${BRANCH_NAME}", selector: upstream()
           script {
-            if (fileExists('_.tinderbox.failed')) {
-              def matcher = readFile('_.tinderbox.failed') =~ "${target}\\.${targetArch} (\\S*) failed, check"
-              def errorLogs = matcher.collect { readFile("_.${target}.${targetArch}.${it[1]}") }.join('\n')
-              error(errorLogs)
+            try {
+              // TODO tmp while testing to make it faster to iterate
+              //copyArtifacts filter: '_.*', projectName: "ci/${BRANCH_NAME}", selector: upstream()
+              copyArtifacts filter: '_.*', projectName: "ci/${BRANCH_NAME}", selector: lastWithArtifacts()
+              if (fileExists('_.tinderbox.failed')) {
+                def matcher = readFile('_.tinderbox.failed') =~ "${target}\\.${targetArch}.*failed, check .*/(.*) for details"
+                def errorLogs = matcher.collect { readFile(it[1]}) }.join('\n')
+                error(errorLogs)
+              }
+            } finally {
+              sh 'rm -f _.*'
             }
           }
         }
